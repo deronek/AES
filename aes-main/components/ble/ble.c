55f44f6cf5f15e8bbe9054dff973b86811e9efa8
MaciejStrzelczyk <m.strzelczyk@onet.eu>
2022-04-27 20:04:07 +0200

    Create ble function

--- aes-main/components/ble/ble.c
+++ aes-main/components/ble/ble.c
@@ -28,10 +28,14 @@ TASK ble_main()
          * - get data from sensor queue
          * - send that data using ble_send_data()
          */
+    
+        xQueuePeek(mpu9255_queue_fifo_data, &malloc_mpu9255_fifo_data, 0);
+        xQueuePeek(hc_sr04_queue_data, &algo_hc_sr04_data, 0);
+
     }
 }
 
 esp_err_t ble_send_data(void *data, uint8_t len)
 {
-
+    esp_ble_gatts_send_indicate(spp_gatts_if, spp_conn_id, spp_handle_table[SPP_IDX_SPP_DATA_NTY_VAL], sizes[i], string[i], false); // change sizes[i] and string[i] 
 }
