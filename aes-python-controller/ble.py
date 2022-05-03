d4e0d6d474bf07ef2e2477dcf41151a6e5a38d04
MaciejStrzelczyk <m.strzelczyk@onet.eu>
2022-05-03 12:36:18 +0200

    receiving data

--- aes-python-controller/ble.py
+++ aes-python-controller/ble.py
@@ -1,7 +1,7 @@
 import asyncio
-import time
-
+from bleak import BleakScanner
 from bleak import BleakClient
+
 
 
 class SensorData:
@@ -18,23 +18,20 @@ class SensorData:
 address = "E8:31:CD:C4:76:62"
 MODEL_NBR_UUID = "0000abf2-0000-1000-8000-00805f9b34fb"
 
-################################ read MAC adress##############################################
+################################ read MAC adress ##############################################
 # async def main():
 #     devices = await BleakScanner.discover()
 #     for d in devices:
 #         print(d)
-#
 # asyncio.run(main())
 
 def callback(sender: int, data: bytearray):
-    print(f"{sender}: {data}")
+   print(f"{sender}: {data}")
 
 async def main(address):
-    async with BleakClient(address) as client:
-        while True:
-            await client.start_notify(MODEL_NBR_UUID, callback)
-
-
+   async with BleakClient(address) as client:
+       while True:
+           await client.start_notify(MODEL_NBR_UUID, callback)
 asyncio.run(main(address))
 
 
@@ -42,12 +39,14 @@ asyncio.run(main(address))
 
 
 
-################################################ read UUID adres ##################################
+################################################ read UUID adress ##################################
 # async def main(address: str):
-#     async with BleakClient(address) as client:
-#         svcs = await client.get_services()
-#         print("Services:")
-#         for service in svcs:
-#             print(service)
-#
+#      async with BleakClient(address) as client:
+#          svcs = await client.get_services()
+#          print("Services:")
+#          for service in svcs:
+#              print(service)
 # asyncio.run(main(address))
+
+if __name__ == "__main__":
+    main()
