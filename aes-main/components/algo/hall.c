2d5fb49cdcf80795b6a58bd0bec7b343c767f8b2
deronek <dionek3@gmail.com>
2022-05-19 21:01:56 +0200

    Add simple hall sensor implementation
    No disturbance and noise assumed on the sensor

---
+++ aes-main/components/algo/hall.c
@@ -0,0 +1,30 @@
+#include "hall.h"
+
+#include "driver/gpio.h"
+
+#define HALL_GPIO_PIN GPIO_NUM_15
+
+// local variables
+static bool detected = false;
+
+/**
+ * @todo Implement some filtering using this interrupt
+ */
+// local function declarations
+static void IRAM_ATTR hall_isr(void *arg)
+{
+    detected = true;
+}
+
+void algo_hall_init()
+{
+    gpio_reset_pin(HALL_GPIO_PIN);
+    gpio_set_direction(HALL_GPIO_PIN, GPIO_MODE_INPUT);
+    gpio_set_intr_type(HALL_GPIO_PIN, GPIO_INTR_POSEDGE);
+    gpio_isr_handler_add(HALL_GPIO_PIN, hall_isr, NULL);
+}
+
+bool algo_get_detected()
+{
+    return detected;
+}
---
+++ aes-main/components/algo/include/hall.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef _HALL_H
+#define _HALL_H
+
+// function declarations
+void algo_hall_init();
+bool algo_get_detected();
+
+#endif
